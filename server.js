12904b75fad10d38215e5460b2649c642b3006c4
Grigory Melnikov <grigory.melnikov@acronis.com>
2018-09-17 11:18:28 +0300

    CoolishReader: add reader for books
    Signed-off-by: Grigory Melnikov <grigory.melnikov@acronis.com>

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var listen = require('./reader/tools/serve.js');
+
 var path = __dirname + "/";
 
 router.use(express.static('public'));
+router.use(express.static('reader/reader'));
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -23,11 +26,17 @@ router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
 
+router.get("/sleep",function(req,res){
+  res.sendFile(path + "reader/reader/reader.html");
+});
+
 app.use("/",router);
 
 app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
+
+listen(app, 3000);
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
