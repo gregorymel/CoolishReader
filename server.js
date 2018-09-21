cf776c6f4d060303a5df7cf6351fbce7de857f76
Mona Lisa <grigory.melnikov@acronis.com>
2018-09-21 03:50:28 +0300

    heroku

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-listen(app, 3000);
+listen(app, process.env.PORT || 3000);
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Live at Port 3000");
 });
